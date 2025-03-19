f517f2707ff8ed57a3d238680c17f1eed8e0655b
Kyan Taber <taberkyan@gmail.com>
2025-03-19 20:48:30 +1100

    Description added

--- bot.py
+++ bot.py
@@ -11,7 +11,14 @@ intents.message_content = True
 
 bot = commands.Bot(
     command_prefix='/',
-    description="FAQ Bot management",
+    description="""
+    🤖 FAQ Bot - Your Automated Assistant
+    ----------------------------------------
+    - Automatically responds to frequently asked questions.
+    - Enhances server engagement by reducing repetitive queries.
+    
+    🔗 GitHub Repository: [FAQ-Bot](https://github.com/KyTDK/FAQ-Bot)
+    """,
     intents=intents,
 )
 
@@ -22,16 +29,15 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
-
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
 @bot.event
 async def on_message(message: discord.Message):
-        answer = qa.find_best_match(message, threshold=0.7)
-        if answer:
-            await message.reply(answer)
+    answer = qa.find_best_match(message, threshold=0.7)
+    if answer:
+        await message.reply(answer)
 
 # --- Run the Bot ---
 if __name__ == "__main__":
